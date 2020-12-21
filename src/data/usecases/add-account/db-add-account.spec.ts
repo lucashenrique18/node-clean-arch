198f0bb15b92bb593c51e59f93499caeb6583918
Lucas Henrique <lucashenrique_18@outlook.com>
2020-12-21 18:04:06 -0300

    test: ensure DbAddAccount calls Hasher hash with correct password

---
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,39 @@
+import { Hasher } from "../../protocols/criptography/hasher";
+import { DbAddAccount } from "./db-add-account";
+
+interface SutTypes {
+  sut: DbAddAccount;
+  hasherStub: Hasher;
+}
+
+const makeHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    async hash(value: string): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_password"));
+    }
+  }
+  return new HasherStub();
+};
+
+const makeSut = (): SutTypes => {
+  const hasherStub = makeHasher();
+  const sut = new DbAddAccount(hasherStub);
+  return {
+    sut,
+    hasherStub,
+  };
+};
+
+describe("DbAddAccount Usecase", () => {
+  test("Should call Hasher hash with correct password", async () => {
+    const { sut, hasherStub } = makeSut();
+    const encryptSpy = jest.spyOn(hasherStub, "hash");
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email@mail.com",
+      password: "valid_password",
+    };
+    await sut.add(accountData);
+    expect(encryptSpy).toHaveBeenCalledWith("valid_password");
+  });
+});
