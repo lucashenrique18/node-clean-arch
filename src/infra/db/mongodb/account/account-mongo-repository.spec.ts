62acfabb13e0c4469aacebf319da8f99936430c9
Lucas Henrique <lucashenrique_18@outlook.com>
2020-12-22 23:05:49 -0300

    test: ensure AccountMongoRepository return an account on add success

---
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -0,0 +1,30 @@
+import { MongoHelper } from "../helpers/mongo-helper";
+import { AccountMongoRepository } from "./account-mongo-repository";
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository();
+};
+
+describe("Account Mongo Repository", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test("Should return an account on add success", async () => {
+    const sut = makeSut();
+    const account = await sut.add({
+      name: "any_name",
+      email: "any_email@mail.com",
+      password: "any_password",
+    });
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe("any_name");
+    expect(account.email).toBe("any_email@mail.com");
+    expect(account.password).toBe("any_password");
+  });
+});
