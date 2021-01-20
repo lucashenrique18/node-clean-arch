c054e83a2a0eb1bb4e8b642d0aeb78a8a2e2017a
Lucas Henrique <lucashenrique_18@outlook.com>
2021-01-20 17:30:30 -0300

    feat: ensure return an account on success

---
+++ src/main/routes/signup-routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+// import { makeSignUpController } from "../factories/signup-factory";
+// import { adaptRoute } from "../adapters/express-route-adapter";
+
+export default (router: Router): void => {
+  router.post("/signup", (req, res) => {
+    res.json({ ok: "ok" });
+  });
+};
