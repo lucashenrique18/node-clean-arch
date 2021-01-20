3865e86c96ea7a67811ce69eac596adc9dba7e5c
Lucas Henrique <lucashenrique_18@outlook.com>
2021-01-20 18:28:15 -0300

    test: ensure LogController call controller handle with correct values

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,61 @@
+import { Controller } from "../../presentation/protocols/controller";
+import { HttpRequest, HttpResponse } from "../../presentation/protocols/http";
+import { LogControllerDecorator } from "./log";
+
+interface SutTypes {
+  sut: Controller;
+  controllerStub: ControllerStub;
+}
+
+class ControllerStub implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse: HttpResponse = {
+      statusCode: 200,
+      body: {
+        name: "any_body",
+      },
+    };
+    return new Promise((resolve) => resolve(httpResponse));
+  }
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: "any_body",
+        },
+      };
+      return new Promise((resolve) => resolve(httpResponse));
+    }
+  }
+  return new ControllerStub();
+};
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController();
+  const sut = new LogControllerDecorator(controllerStub);
+  return {
+    sut,
+    controllerStub,
+  };
+};
+
+describe("LogController Decorator", () => {
+  test("Should call controller handle", async () => {
+    const { sut, controllerStub } = makeSut();
+    const handleSpy = jest.spyOn(controllerStub, "handle");
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_mail@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+});
