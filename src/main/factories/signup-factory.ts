80895f72d3b9394e9bbd807c9d30f7aae336de68
Lucas Henrique <lucashenrique_18@outlook.com>
2021-01-20 18:27:28 -0300

    refactor: move controller to a decorator

--- src/main/factories/signup-factory.ts
+++ src/main/factories/signup-factory.ts
@@ -2,13 +2,19 @@ import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account/account-mongo-repository";
 import { SignUpController } from "../../presentation/controllers/signup/signup-controller";
+import { Controller } from "../../presentation/protocols/controller";
 import { EmailValidatorAdapter } from "../../utils/email-validator/email-validator-adapter";
+import { LogControllerDecorator } from "../decorators/log";
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12;
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const hasherAdapter = new BcryptAdapter(salt);
   const accountMongoRepository = new AccountMongoRepository();
   const dbAddAccount = new DbAddAccount(hasherAdapter, accountMongoRepository);
-  return new SignUpController(emailValidatorAdapter, dbAddAccount);
+  const signUpController = new SignUpController(
+    emailValidatorAdapter,
+    dbAddAccount
+  );
+  return new LogControllerDecorator(signUpController);
 };
