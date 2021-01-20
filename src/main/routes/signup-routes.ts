1c3236fe46ee563dea77cd606229eca77e806530
Lucas Henrique <lucashenrique_18@outlook.com>
2021-01-20 17:43:03 -0300

    refactor: use composition of signup factory

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
-// import { makeSignUpController } from "../factories/signup-factory";
-// import { adaptRoute } from "../adapters/express-route-adapter";
+import { makeSignUpController } from "../factories/signup-factory";
+import { adaptRoute } from "../adapters/express-route-adapter";
 
 export default (router: Router): void => {
-  router.post("/signup", (req, res) => {
-    res.json({ ok: "ok" });
-  });
+  router.post("/signup", adaptRoute(makeSignUpController()));
 };
